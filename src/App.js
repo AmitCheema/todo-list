33cdd542631f082190f499bd4e8eb3c0bc4fbdd4
amit238 <50054338+amit238@users.noreply.github.com>
2023-02-28 13:36:12 +0000

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  
+  const addTodo = (text) => {
+    let id = 1;
+    if(todos.length > 0) {
+      id = todos[0].id + 1
+    }
+    let todo = {id: id, text: text, completed: false, important: false}
+    let newTodos = [todo, ...todos]
+    setTodos(newTodos)
+  };
+  const removeTodo = (id) => {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
   return (
     <div className="todo-app">
-
+      <h1>Todo List </h1>
+      <TodoForm addTodo={addTodo} />
+      <hr className="seperate"/>
+      {todos.map((todo) => {
+        return (
+          <TodoItem removeTodo={removeTodo} completeTodo={completeTodo} todo={todo} key={todo.id}/>
+        )
+      })}
     </div>
   );
 }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    props.addTodo(input)
+    setInput("")
+  }
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        className="todo-input"
+        placeholder="Add a todo"
+      ></input>
+      <button type="submit" className="todo-button">Add</button>
+    </form>
+  );
+}
